4a74fe01ffac345b292396f6d092480bba4d0ab6
DomanTom07 <tamasdoman7@citromail.hu>
2022-10-28 17:24:52 +0200

    Feladat kész

--- src/App.java
+++ src/App.java
@@ -1,5 +1,8 @@
 public class App {
     public static void main(String[] args) throws Exception {
+        NameGetter nameGetter = new NameGetter();
+        String[] nevek = {"Béla", "Géza"};
 
+        nameGetter.getNames(nevek);
     }
 }
---
+++ src/NameGetter.java
@@ -0,0 +1,7 @@
+public class NameGetter implements Name {
+    public void getNames(String[] nevek) {
+        for (String nev : nevek) {
+            System.out.println(nev);
+        }
+    }
+}
